e7679c4a2b2f624e94194fa9af7c41806657345a
Rhytah <ritanamono@gmail.com>
2019-02-11 00:29:00 +0300

    [Delivers #163696381]  can fetch and view both types of incident records

--- static/js/get_incidents.js
+++ static/js/get_incidents.js
@@ -1,10 +1,13 @@
 const get_redflag_url = 'https://rhytah-ireporterv2.herokuapp.com/api/v2/red-flags/'
+const get_intervention_url = 'https://rhytah-ireporterv2.herokuapp.com/api/v2/interventions/'
+
 
 document.getElementById('getredflags').addEventListener('click', refreshRedflags);
+document.getElementById('getinterventions').addEventListener('click', refreshInterventions);
+
 function refreshRedflags(){
     let invalid = document.getElementById('invalid');
-    let tableBody = document.querySelector('#redflag_records > tbody');
-
+ 
     fetch(get_redflag_url, {
         method: 'GET',
         mode: 'cors',
@@ -31,6 +34,42 @@ function refreshRedflags(){
         `;
             });
             document.getElementById('output').innerHTML = output;
+        }
+        console.log(data)
+    })
+    .catch((error) => console.log(error), invalid.textContent = "Ooops. Something went wrong");
+}
+
+// fetch interventions
+function refreshInterventions(){
+    let invalid = document.getElementById('invalid');
+ 
+    fetch(get_intervention_url, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if(data.message === 'These are the Intervention records'){
+        let output1 ='<h2>Interventions</h2>';
+        let interventions = data["data"];
+        interventions.forEach(function(intervention){
+        output1 += `
+        <ul>
+        <li>ID: ${intervention.intervention_id}</li>
+        <li>UserID: ${intervention.created_by}</li>
+        <li>Date: ${intervention.created_on}</li>
+        <li>image: ${intervention.image}</li>
+        <li>video: ${intervention.video}</li>
+        <li>Latitude: ${intervention.lat}</li>
+        <li> Longitude: ${intervention.long}</li>
+        <li> Status: ${intervention.status}</li>
+        <li> Comment: ${intervention.comment}</li>
+        </ul>
+        `;
+            });
+            document.getElementById('output1').innerHTML = output1;
         }
         console.log(data)
     })
